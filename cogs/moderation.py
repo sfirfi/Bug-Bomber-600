0e670f780488e5a63b7797425bc288ec3bb0d7af
Unknown <artmazingmao@gmail.com>
2018-06-18 18:40:25 -0400

    Trying a dirty trick.

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -292,6 +292,8 @@ class ModerationCog:
             until = time.time() + duration
             await ctx.guild.ban(member, reason=f"Moderator: {ctx.author.name} ({ctx.author.id}), Duration: {durationNumber}{durationIdentifier} Reason: {reason}")
             await ctx.send(f":ok_hand: {member.name} ({member.id}) has been banned for {durationNumber}{durationIdentifier}")
+            await asyncio.sleep(durationNumber)
+            await ctx.guild.unban(member, reason=f"Moderator: {ctx.author.name} ({ctx.author.id}). Their temporary ban has expired.")
 
     @commands.command()
     @commands.guild_only()
